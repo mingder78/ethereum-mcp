c4e022601dd724d961269b32b0167496ca489742
ming-der wang <mingderwang@me.com>
2025-03-29 20:47:26 +0800

    tmp

--- src/ethereum_mcp/cli.py
+++ src/ethereum_mcp/cli.py
@@ -1,12 +1,13 @@
 from mcp.server.fastmcp import FastMCP
-import sys
 
 mcp = FastMCP("My App")
+
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
 def main():
-    if len(sys.argv) > 1:
-        print(f"Hello, {sys.argv[1]}!")
-    else:
-        print("Hello, world!")
     mcp.run()
 
 if __name__ == "__main__":
